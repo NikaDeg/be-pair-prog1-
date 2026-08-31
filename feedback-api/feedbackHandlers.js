8e48b368da5c5fdcd4be8a1badf47474b552979e
NikaDeg <garelskaiavv1@gmail.com>
2026-08-31 14:48:46 +0300

    chore: initialize feedback API

---
+++ feedback-api/feedbackHandlers.js
@@ -0,0 +1,28 @@
+
+const getAllFeedbacks = (req, res) => {
+  res.status(200).json({ message: "Get all feedbacks" });
+};
+
+const createFeedback = (req, res) => {
+    res.status(201).json({ message: "Create a new feedback" });
+}
+
+const getFeedbackById = (req, res) => {
+    const { feedbackId } = req.params;
+    res.status(200).json({ message: `Get feedback with ID: ${feedbackId}` });
+}
+const updateFeedback = (req, res) => {
+    const { feedbackId } = req.params;
+    res.status(200).json({ message: `Update feedback with ID: ${feedbackId}` });
+}
+const deleteFeedback = (req, res) => {
+    const { feedbackId } = req.params;
+    res.status(200).json({ message: `Delete feedback with ID: ${feedbackId}` });
+}
+
+module.exports = { getAllFeedbacks,
+createFeedback,
+getFeedbackById,
+updateFeedback,
+deleteFeedback, 
+};
