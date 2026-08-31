023a3adf797c7a1c2ca99916e078683262a67eda
NikaDeg <garelskaiavv1@gmail.com>
2026-08-31 18:06:30 +0300

    feat(api): add tour request handlers

--- tour-api/tourHandlers.js
+++ tour-api/tourHandlers.js
@@ -6,26 +6,26 @@ const getAllTours = (req, res) => {
 };
 
 
-// const createTour = (req, res) => {
-//   res.json({ message: "Hello from createTour" });
-// };
+const createTour = (req, res) => {
+  res.json({ message: "Hello from createTour" });
+};
 
-// const getTourById = (req, res) => {
-//   res.json({ message: "Hello from getTourById" });
-// };
+const getTourById = (req, res) => {
+  res.json({ message: "Hello from getTourById" });
+};
 
-// const updateTour = (req, res) => {
-//   res.json({ message: "Hello from updateTour" });
-// };
+const updateTour = (req, res) => {
+  res.json({ message: "Hello from updateTour" });
+};
 
-// const deleteTour = (req, res) => {
-//   res.json({ message: "Hello from deleteTour" });
-// };
+const deleteTour = (req, res) => {
+  res.json({ message: "Hello from deleteTour" });
+};
 
 module.exports = {
   getAllTours,
-  //createTour,
-  //getTourById,
-  //updateTour,
-  //deleteTour,
+  createTour,
+  getTourById,
+  updateTour,
+  deleteTour,
 };
