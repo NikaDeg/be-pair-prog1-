e6bcdbe8482ab16c22a8401044bd4e69b130ca15
ddmtrung <trungdao200703@gmail.com>
2026-09-02 15:43:09 +0300

    Upload Pagelinks and ..link

--- week3-fe-pair-programming/src/components/Footer.jsx
+++ week3-fe-pair-programming/src/components/Footer.jsx
@@ -1,29 +1,11 @@
+import PageLinks from './PageLinks';
+import Pagelink from './PageLink';
+
 function Footer() {
   return (
     <div>
          <footer className="section footer">
-            <ul className="footer-links" id="nav-links">
-              <li>
-                <a href="#home" className="footer-link">
-                  home
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="footer-link">
-                  about
-                </a>
-              </li>
-              <li>
-                <a href="#services" className="footer-link">
-                  services
-                </a>
-              </li>
-              <li>
-                <a href="#tours" className="footer-link">
-                  tours
-                </a>
-              </li>
-            </ul>
+            <PageLinks parentClass='footer-links' itemClass='footer-link' />
             <ul className="footer-icons">
               <li>
                 <a
