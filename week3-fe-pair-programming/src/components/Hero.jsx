f2c0e1511f270204ad83e7301665e6e05e1e7d1a
NikaDeg <garelskaiavv1@gmail.com>
2026-09-02 16:25:00 +0300

    Apply css-style for Hero component

--- week3-fe-pair-programming/src/components/Hero.jsx
+++ week3-fe-pair-programming/src/components/Hero.jsx
@@ -1,21 +1,22 @@
-function Hero() {
+import React from 'react';
+import styles from '../Hero.module.css'; 
+
+const Hero = () => {
   return (
-    <div>
-      <section className="hero" id="home">
-        <div className="hero-banner">
-          <h1>backroads app</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo
-            debitis est autem dicta.
-          </p>
-          <a href="#tours" className="btn hero-btn">
-            explore tours
-          </a>
-        </div>
-      </section>
-    </div>
-  );
+    <section className={styles.hero} id='home'>
+      <div className={styles['hero-banner']}>
+        <h1>backroads app</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
+        </p>
+        <a href='#tours' className={`btn ${styles['hero-btn']}`}>
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
 }
 
-export default Hero;
+export default Hero
  
